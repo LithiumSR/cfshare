4a6ac2106389f3ae61ed4fc91210804676a92b3c
Leonardo Sarra <leonardosarra@outlook.com>
2020-10-22 18:43:14 +0200

    Change old references to crypto_split in setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ __email__ = "leonardosarra@outlook.com"
 from setuptools import setup
 
 setup(
-    name='crypto_split',
-    version="0.1",
+    name='cfshare',
+    version="1.0",
     license=__license__,
-    description='Split files in multiple encrypted fragments and reconstruct the original file from a subset of the fragments',
+    description='Split files in multiple encrypted shares and reconstruct the original file from a subset of them',
     author=__author__,
     author_email=__email__,
-    url='https://github.com/lithiumsr/crypto_split',
-    download_url='https://github.com/lithiumsr/crypto_split/archive/master.tar.gz',
-    package_dir={'crypto_split': 'crypto_split', 'secret_sharing': 'secret_sharing'},
+    url='https://github.com/lithiumsr/cfshare',
+    download_url='https://github.com/lithiumsr/cfshare/archive/master.tar.gz',
+    package_dir={'cfshare': 'cfshare', 'secret_sharing': 'secret_sharing'},
     packages=['cfshare', 'secret_sharing'],
     install_requires=[
         'cryptography',
